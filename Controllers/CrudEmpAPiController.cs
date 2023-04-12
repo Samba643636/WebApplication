9321b0d3d97d5cc4555f03be55405e01ddb3e7f0
samba643636 <sambayenumula83@gmail.com>
2023-04-12 10:33:08 +0530

    First commit

---
+++ Controllers/CrudEmpAPiController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class CrudEmpAPiController : ApiController
+    {
+        WebApi_CrudEntities db = new WebApi_CrudEntities();
+        [HttpGet]
+        public IHttpActionResult GetEmployees()
+        {
+            try
+            {
+                List<Employee> list = db.Employees.ToList();
+                return Ok(list);
+            }catch(Exception ex)
+            {
+                return null;
+            }
+        }
+        [HttpPost]
+        public IHttpActionResult EmpInsert(Employee e)
+        {
+            try
+            {
+                db.Employees.Add(e);
+                db.SaveChanges();
+                return Ok();
+            }catch(Exception ex)
+            {
+                return null;
+            }
+        }
+        [HttpGet]
+        public IHttpActionResult GetEmployeesById(int Emp_id)
+        {
+            try
+            {
+                var Emp = db.Employees.Where(x => x.Emp_id == Emp_id).FirstOrDefault();
+                return Ok(Emp);
+            }catch(Exception ex)
+            {
+                return null;
+            }
+        }
+        [HttpPut]
+        public IHttpActionResult EmpUpdate(Employee e)
+        {
+            try
+            {
+                var emp = db.Employees.Where(x => x.Emp_id == e.Emp_id).FirstOrDefault();
+                if (emp != null)
+                {
+                    emp.Emp_id = e.Emp_id;
+                    emp.Emp_Name = e.Emp_Name;
+                    emp.Emp_gender = e.Emp_gender;
+                    emp.Emp_age = e.Emp_age;
+                    emp.Emp_designation = e.Emp_designation;
+                    emp.Emp_Salary = e.Emp_Salary;
+                    db.SaveChanges();
+                }
+                else
+                {
+                    return NotFound();
+                }
+
+                return Ok();
+            }catch(Exception ex)
+            {
+                return null;
+            }
+        }
+        [HttpDelete]
+        public IHttpActionResult EmpDelete(int Emp_id)
+        {
+            try
+            {
+                var emp = db.Employees.Where(x => x.Emp_id == Emp_id).FirstOrDefault();
+                //db.Entry(emp).State = System.Data.Entity.EntityState.Deleted;
+                db.Employees.Remove(emp);
+                db.SaveChanges();
+                return Ok();
+            }catch(Exception ex)
+            {
+                return null;
+            }
+        }
+        [HttpGet]
+        public IHttpActionResult EmpSearch(string EmpName)
+        {
+            try
+            {
+                var emp = db.Employees.Where(x => x.Emp_Name == EmpName).ToList();
+                return Ok(emp);
+            }catch(Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ Controllers/CrudEmpMvcController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class CrudEmpMvcController : Controller
+    {
+        // GET: CrudEmpMvc
+        HttpClient client = new HttpClient();
+        public ActionResult Index()
+        {
+            List<Employee> emp_list = new List<Employee>();
+            client.BaseAddress = new Uri("https://localhost:44380/api/CrudEmpAPi");
+            var response = client.GetAsync("CrudEmpAPi");
+            response.Wait();
+
+            var test = response.Result;
+            if(test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<List<Employee>>();
+                display.Wait();
+                emp_list = display.Result;
+            }
+
+            return View(emp_list);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Employee e)
+        {
+            if (ModelState.IsValid)
+            {
+                client.BaseAddress = new Uri("https://localhost:44380/api/CrudEmpAPi");
+                var response = client.PostAsJsonAsync<Employee>("CrudEmpAPi", e);
+                response.Wait();
+
+                var test = response.Result;
+                if (test.IsSuccessStatusCode)
+                {
+                    TempData["alertMessage"] = "Employee Details Added sucessfully";
+                    return RedirectToAction("Index");
+                }
+                return View("Create");
+            }
+            else
+            {
+                return View("Create");
+            }
+        }
+        [HttpGet]
+        public ActionResult Details(int Emp_id)
+        {
+            Employee e = null;
+            client.BaseAddress = new Uri("https://localhost:44380/api/CrudEmpAPi");
+            var response = client.GetAsync("CrudEmpAPi?Emp_id=" + Emp_id.ToString());
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<Employee>();
+                display.Wait();
+                e = display.Result;
+            }
+            return View(e);
+        }
+        [HttpGet]
+        public ActionResult Edit(int Emp_id)
+        {
+            Employee e = null;
+            client.BaseAddress = new Uri("https://localhost:44380/api/CrudEmpAPi");
+            var response = client.GetAsync("CrudEmpAPi?Emp_id=" + Emp_id.ToString());
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<Employee>();
+                display.Wait();
+                e = display.Result;
+            }
+            return View(e);
+        }
+        [HttpPost]
+        public ActionResult Edit(Employee e)
+        {
+            if (ModelState.IsValid)
+            {
+                client.BaseAddress = new Uri("https://localhost:44380/api/CrudEmpAPi");
+                var response = client.PutAsJsonAsync<Employee>("CrudEmpAPi", e);
+                response.Wait();
+
+                var test = response.Result;
+                if (test.IsSuccessStatusCode)
+                {
+                    TempData["alertMessage"] = "Employee Details Updated sucessfully";
+                    return RedirectToAction("Index");
+                }
+                return View("Edit");
+            }
+            else
+            {
+                return View("Edit");
+            }
+        }
+       
+        public ActionResult Delete(int Emp_id)
+        {
+            Employee e = null;
+            client.BaseAddress = new Uri("https://localhost:44380/api/CrudEmpAPi");
+            var response = client.GetAsync("CrudEmpAPi?Emp_id=" + Emp_id.ToString());
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<Employee>();
+                display.Wait();
+                e = display.Result;
+            }
+            return View(e);
+        }
+        [HttpPost,ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int Emp_id)
+        {
+            client.BaseAddress = new Uri("https://localhost:44380/api/CrudEmpAPi");
+            var response = client.DeleteAsync("CrudEmpAPi?Emp_id=" + Emp_id.ToString());
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                TempData["alertMessage"] = "Employee Details Deleted sucessfully";
+                return RedirectToAction("Index");
+            }
+            return View("Delete");
+        }
+        
+        public ActionResult EmpSearch(string EmpName)
+        {
+            List<Employee> e = new List<Employee>();
+            client.BaseAddress = new Uri("https://localhost:44380/api/CrudEmpAPi");
+            var response = client.GetAsync("CrudEmpAPi?EmpName=" + EmpName);
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<List<Employee>>();
+                display.Wait();
+                e = display.Result;
+                if (e.Count != 0)
+                {
+                    return View(e);
+                }
+                else
+                {
+                    ViewBag.message = "No Records Found";
+                    return View();
+                }
+            }
+            return View();
+        }
+    }
+}
